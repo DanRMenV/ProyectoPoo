944e59c0250a600bcb338212f067865865d0f109
Mendivelso <Mendivelso@Mendivelso-PC>
2019-02-27 15:06:29 -0500

    add get a set

--- ProyectoParking/src/business/ParkingManager.java
+++ ProyectoParking/src/business/ParkingManager.java
@@ -1,6 +1,21 @@
 package business;
 
-public class ParkingManager {
+import data.Vehicle;
 
+public class ParkingManager {
+	
+	Vehicle[] vehicles=new Vehicle[30];
+
+	public Vehicle[] getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(Vehicle[] vehicles) {
+		this.vehicles = vehicles;
+	}
+	
+	
+	
+	
 	
 }
