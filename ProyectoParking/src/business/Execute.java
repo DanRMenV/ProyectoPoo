1ee3f3470dc70827e8b75ffc0348a5bc63063a3e
Mendivelso <Mendivelso@Mendivelso-PC>
2019-02-27 15:58:19 -0500

    nothing

--- ProyectoParking/src/business/Execute.java
+++ ProyectoParking/src/business/Execute.java
@@ -1,7 +1,5 @@
 package business;
 
-import data.Vehicle;
-
 public class Execute {
 
 	public static void main(String[] args) {
--- ProyectoParking/src/data/Motorcycle.java
+++ ProyectoParking/src/data/Motorcycle.java
@@ -2,8 +2,6 @@ package data;
 
 public class Motorcycle extends Vehicle {
 	
-	
-	
 	public Motorcycle(String marca, String placa) {
 		super(marca, placa);
 		// TODO Auto-generated constructor stub
