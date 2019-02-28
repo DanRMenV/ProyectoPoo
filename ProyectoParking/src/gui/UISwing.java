70f082ab1ac4dd8bc9983631ab09a71690500483
dmendivelso <dmendivelso@BE401S204C22ING.unal.loc>
2019-02-28 10:24:18 -0500

    updated

--- ProyectoParking/src/gui/UISwing.java
+++ ProyectoParking/src/gui/UISwing.java
@@ -1,7 +1,29 @@
 package gui;
 
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+
 import business.ParkingManager;
 
-public class UISwing {
+public class UISwing extends JFrame{
+	
+	private JPanel mainPanel;
 	private ParkingManager pm;
+	
+	public UISwing(ParkingManager pm) {
+		this.pm=pm;
+		this.setTitle("MyContacts");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocation(200, 100);
+		this.setResizable(false);
+		this.mainPanel=new JPanel();
+		this.mainPanel.setBorder(new EmptyBorder(10,10,10,10));
+		this.mainPanel.setLayout(new CardLayout(0,0));
+		this.setContentPane(mainPanel);
+		//initComponents();
+		this.setVisible(true);
+	}
+	
 }
