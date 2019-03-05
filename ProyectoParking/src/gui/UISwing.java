c1744a04ff185a5c305f800afb05993a4a82fc17
Brian <Brian@DESKTOP-CJG4D6I>
2019-03-04 21:56:04 -0500

    Menu

--- ProyectoParking/src/gui/UISwing.java
+++ ProyectoParking/src/gui/UISwing.java
@@ -1,29 +1,165 @@
 package gui;
 
-import java.awt.*;
+import java.awt.CardLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-import javax.swing.*;
-import javax.swing.border.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 import business.ParkingManager;
+import data.Car;
+import data.Motorcycle;
+import data.Vehicle;
 
 public class UISwing extends JFrame{
 	
 	private JPanel mainPanel;
+	private JPanel panelMenu;
+	private JPanel panelIngreso;
+	private JPanel panelEstado;
+	private JPanel panelPagos;
+	private JPanel panelStats;
+	private JPanel panelPrecios;
+	private JPanel panelRegistro;
+	private JPanel panelFacturas;
+	
+	
 	private ParkingManager pm;
 	
 	public UISwing(ParkingManager pm) {
 		this.pm=pm;
-		this.setTitle("MyContacts");
+		this.setTitle("ParkingManager");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocation(200, 100);
+		this.setLocation(200,100);
 		this.setResizable(false);
-		this.mainPanel=new JPanel();
-		this.mainPanel.setBorder(new EmptyBorder(10,10,10,10));
+		this.mainPanel = new JPanel ();
+		this.mainPanel.setBorder(new EmptyBorder (10,10,10,10));
 		this.mainPanel.setLayout(new CardLayout(0,0));
 		this.setContentPane(mainPanel);
-		//initComponents();
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		mainPanel.setLayout(gbl_contentPane);
+		initComponents();
 		this.setVisible(true);
 	}
+	
+	private void initComponents() {
+		this.setupPanelMenu();
+		this.setupPanelIngreso();
+		this.setupPanelEstado();
+		this.setupPanelPagos();
+		this.setupPanelStats();
+		this.setupPanelPrecios();
+		this.setupPanelRegistro();
+		this.setupPanelFactura();	
+	}
+	
+	//Setup menu
+	private void setupPanelMenu() {
+		this.panelMenu = new JPanel();
+		JLabel label = new JLabel("Parking");
+		label.setFont(new Font("Bodoni MT", Font.PLAIN, 24));
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 5;
+		gbc_label.gridy = 0;
+		mainPanel.add(label, gbc_label);
+		
+		JLabel lblHoraActual = new JLabel("Hora actual");
+		lblHoraActual.setFont(new Font("Bodoni MT", Font.PLAIN, 12));
+		GridBagConstraints gbc_lblHoraActual = new GridBagConstraints();
+		gbc_lblHoraActual.insets = new Insets(0, 0, 5, 0);
+		gbc_lblHoraActual.gridx = 9;
+		gbc_lblHoraActual.gridy = 0;
+		mainPanel.add(lblHoraActual, gbc_lblHoraActual);
+		
+		JButton btnIngreso = new JButton("Ingreso");
+		GridBagConstraints gbc_btnIngreso = new GridBagConstraints();
+		gbc_btnIngreso.insets = new Insets(0, 0, 5, 5);
+		gbc_btnIngreso.gridx = 1;
+		gbc_btnIngreso.gridy = 2;
+		mainPanel.add(btnIngreso, gbc_btnIngreso);
+		
+		JButton btnEstado = new JButton("Estado");
+		GridBagConstraints gbc_btnEstado = new GridBagConstraints();
+		gbc_btnEstado.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEstado.gridx = 5;
+		gbc_btnEstado.gridy = 2;
+		mainPanel.add(btnEstado, gbc_btnEstado);
+		
+		JButton btnPagos = new JButton("Pagos");
+		GridBagConstraints gbc_btnPagos = new GridBagConstraints();
+		gbc_btnPagos.insets = new Insets(0, 0, 5, 0);
+		gbc_btnPagos.gridx = 9;
+		gbc_btnPagos.gridy = 2;
+		mainPanel.add(btnPagos, gbc_btnPagos);
+		
+		JButton btnRegistroClienteFrecuente = new JButton("Registro Cliente Frecuente");
+		GridBagConstraints gbc_btnRegistroClienteFrecuente = new GridBagConstraints();
+		gbc_btnRegistroClienteFrecuente.insets = new Insets(0, 0, 5, 5);
+		gbc_btnRegistroClienteFrecuente.gridx = 5;
+		gbc_btnRegistroClienteFrecuente.gridy = 5;
+		mainPanel.add(btnRegistroClienteFrecuente, gbc_btnRegistroClienteFrecuente);
+		
+		JButton btnStats = new JButton("Stats");
+		GridBagConstraints gbc_btnStats = new GridBagConstraints();
+		gbc_btnStats.insets = new Insets(0, 0, 5, 5);
+		gbc_btnStats.gridx = 1;
+		gbc_btnStats.gridy = 6;
+		mainPanel.add(btnStats, gbc_btnStats);
+		
+		JButton btnPrecios = new JButton("Precios");
+		GridBagConstraints gbc_btnPrecios = new GridBagConstraints();
+		gbc_btnPrecios.insets = new Insets(0, 0, 5, 0);
+		gbc_btnPrecios.gridx = 9;
+		gbc_btnPrecios.gridy = 6;
+		mainPanel.add(btnPrecios, gbc_btnPrecios);
+		
+		JButton btnSalidaPrograma = new JButton("Salida Programa");
+		GridBagConstraints gbc_btnSalidaPrograma = new GridBagConstraints();
+		gbc_btnSalidaPrograma.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSalidaPrograma.gridx = 5;
+		gbc_btnSalidaPrograma.gridy = 7;
+		mainPanel.add(btnSalidaPrograma, gbc_btnSalidaPrograma);	
+	
+	// Listeners
+			btnIngreso.addActionListener(new ActionListener () {
+				public void actionPerformed (ActionEvent e) {
+					remove(panelMenu);
+					showPanelIngreso();
+				}
+				});
+			
+			btnSelect.addActionListener(new ActionListener () {
+				public void actionPerformed (ActionEvent e) {
+					remove(panelMenu);
+					selectContact(cmr.getIds());
+				}
+				});
+			
+			btnExit.addActionListener(new ActionListener () {
+				public void actionPerformed (ActionEvent e) {
+					remove(panelWelcome);
+					System.exit(0);;
+				}
+				});
+		}
 	
 }
